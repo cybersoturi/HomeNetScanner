a8e2d2c61dd62aaf8928ebdf48385dadfe61195b
cybersoturi <pasi.hakkarainen@iki.fi>
2016-12-27 09:15:15 +0200

    Minor update
    Fixed sorting

--- BasicScan.sh
+++ BasicScan.sh
@@ -5,4 +5,4 @@
 LAN=$(ifconfig | grep Bcast|cut -d":" -f3 | cut -d" " -f1)
 
 #scan all possible LAN IP address' and write a sorted list of identified IPs and MACs
-nmap -sP $LAN/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}' | sort $ > /foo/bar/result.list
+nmap -sP $LAN/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}' | sort -V > /foo/bar/result.list
