7c7cf8dc2bb7e1d167b859d28cbdb1a9b397ab39
cybersoturi <pasi.hakkarainen@iki.fi>
2016-12-27 12:33:17 +0200

    Update BasicScan.sh
    Fixed a bug where scanning host did not get it's own MAC address

--- BasicScan.sh
+++ BasicScan.sh
@@ -2,7 +2,14 @@
 #home network scan
 
 #identify LAN Broadcast IP
-LAN=$(ifconfig | grep Bcast|cut -d":" -f3 | cut -d" " -f1)
+LAN=$(ifconfig|grep Bcast|cut -d":" -f3|cut -d" " -f1)
 
 #scan all possible LAN IP address' and write a sorted list of identified IPs and MACs
-nmap -sP $LAN/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}' | sort -V > /foo/bar/result.list
+nmap -sP $LAN/24|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/h$
+
+#this list still missees scanner host MAC address
+#get the host IP and MAC and add it to correct place in the list
+ownIP=$(ifconfig|grep Bcast|cut -d":" -f2|cut -d" " -f1)
+ownMAC=$(ifconfig|grep HWaddr|cut -d" " -f11)
+
+sed -i 's/\b'"$ownIP"'\b/&'" $ownMAC"'/' /home/pi/hybrid/result.list
