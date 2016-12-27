29f1ae294d165a66e8499b5177d1675ca19e9167
cybersoturi <pasi.hakkarainen@iki.fi>
2016-12-27 13:46:36 +0200

    Update BasicScan.sh

--- BasicScan.sh
+++ BasicScan.sh
@@ -5,11 +5,11 @@
 LAN=$(ifconfig|grep Bcast|cut -d":" -f3|cut -d" " -f1)
 
 #scan all possible LAN IP address' and write a sorted list of identified IPs and MACs
-nmap -sP $LAN/24|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/h$
+nmap -sP $LAN/24|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/result.list
 
 #this list still missees scanner host MAC address
 #get the host IP and MAC and add it to correct place in the list
 ownIP=$(ifconfig|grep Bcast|cut -d":" -f2|cut -d" " -f1)
 ownMAC=$(ifconfig|grep HWaddr|cut -d" " -f11)
 
-sed -i 's/\b'"$ownIP"'\b/&'" $ownMAC"'/' /home/pi/hybrid/result.list
+sed -i 's/\b'"$ownIP"'\b/&'" $ownMAC"'/' /home/pi/result.list
