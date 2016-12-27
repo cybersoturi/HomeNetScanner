482a244392863aada4f389ecc931f375e15b2c28
cybersoturi <pasi.hakkarainen@iki.fi>
2016-12-27 08:57:31 +0200

    Update BasicScan.sh

--- BasicScan.sh
+++ BasicScan.sh
@@ -5,4 +5,4 @@
 range=$(ifconfig|grep Bcast|cut -d":" -f3|cut -d" " -f1)
 
 #finding hosts and MAC with NMAP ping scan and writing them to list
-nmap -sP $range/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}' | sort $ >> /foo/bar/result.list
+nmap -sP $range/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}' | sort $ > /foo/bar/result.list
