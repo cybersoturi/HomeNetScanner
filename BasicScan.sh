936ca5fca59fc0d36d4504173a17002763f62353
cybersoturi <pasi.hakkarainen@iki.fi>
2017-04-22 19:13:05 +0300

    Update BasicScan.sh

--- BasicScan.sh
+++ BasicScan.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-#home network scan
-#version 1.0.1, 20.3.2017
-
-#identify host's LAN IPv4 address
+#home network scan version 1.0.2, 20.3.2017
+#identify host's LAN IPv4 address v 1.0.0
 LAN=$(ifconfig|grep Bcast|cut -d":" -f2|cut -d" " -f1)
 
-#to optimize, identify the subnet mask
+#to optimize, identify the subnet mask, can be /24 or smaller v 1.0.2
 MASK=$(ifconfig|grep Bcast|cut -d":" -f4|cut -d"." -f4)
 
-#scan all possible LAN IP address' and write a sorted list of identified IPs and MACs
-nmap -sP $LAN/24|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/result.list
+D2B=({0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1})
+ARVO=${D2B[$MASK]}
+ARVO=$(echo $ARVO | cut -d"0" -f1 | wc -c)
+MASK=$((23+$ARVO))
+
+
+#scan all possible LAN IP address' and write a sorted list of identified IPs and MACs v 1.0.0
+nmap -sP $LAN/$MASK|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/result.list
 
 #this list still miss scanner host,s MAC address
-#get the host IP and MAC and add it to correct place in the list
+#get the host IP and MAC and add it to correct place in the list v 1.0.1
 ownIP=$(ifconfig|grep Bcast|cut -d":" -f2|cut -d" " -f1)
 ownMAC=$(ifconfig|grep HWaddr|cut -d" " -f11)
 
 sed -i 's/\b'"$ownIP"'\b/&'" $ownMAC"'/' /home/pi/result.list
+
