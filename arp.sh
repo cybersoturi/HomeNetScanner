89147db440834af16d4bf948c9c04f2789df24f3
cybersoturi <pasi.hakkarainen@iki.fi>
2017-04-30 19:27:31 +0300

    Update arp.sh

--- arp.sh
+++ arp.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-#yellow light
-./lights.sh 2
-
-arp |awk '/ether/{printf $1;}/ether/{print " "$3;}'|sort -V > /home/pi/netskanner/arp.list
-
-#green light
-./lights.sh 3
+arp |awk '/ether/{printf $1;}/ether/{print " "$3;}'|sort -V > /home/pi/arp.list
