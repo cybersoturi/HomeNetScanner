4bfe65ec1c77704d3da708f18f9ad85b9b1afe45
cybersoturi <pasi.hakkarainen@iki.fi>
2017-04-30 13:23:44 +0300

    Update lights.sh

--- lights.sh
+++ lights.sh
@@ -5,35 +5,43 @@
 #./lights.sh 0 for red and alarm, 2 for yellow and 3 for green
 
 #Set up all pins to output: red, yellow, green, audio
-source gpio 
-gpio mode 0 out  
-gpio mode 2 out  
-gpio mode 3 out  
-gpio mode 4 out  
+
+gpio mode 0 out
+gpio mode 2 out
+gpio mode 3 out
+gpio mode 4 out
+
+gpio write 0 0
+gpio write 2 0
+gpio write 3 0
+gpio write 4 0
 
 #0, alarm, red & audio
-if  [ $1=0 ]
+if  [ $1 -eq 0 ]
 then
-while true; do 
+while true; do
     gpio write 0 1
-    sleep 0.5
-    gpio write 0 2
-    sleep 0.5
-done  
+    gpio write 4 1
+    sleep 1
+    gpio write 0 0
+    gpio write 4 0
+    sleep 1
+
+done
 
 #2 or other value, scanning, yellow
-elif
-    gpio write 2 1
+elif [ $1 -eq 2 ]
 then
+    gpio write 2 1
 
 #3, OK, green
-elif  [ $1=0 ]
+elif  [ $1 -eq 3 ]
 then
     gpio write 3 1
 
 #other value, problem, blinking all
 else
-while true; do 
+while true; do
     gpio write 0 1
     gpio write 2 1
     gpio write 3 1
@@ -42,7 +50,6 @@ while true; do
     gpio write 2 0
     gpio write 3 0
     sleep 0.5
-done  
-
+done
 
 fi
