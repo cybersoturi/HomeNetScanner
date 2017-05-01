46288ad49518b001e28aabd236dbaa57d9a5ef05
cybersoturi <pasi.hakkarainen@iki.fi>
2017-05-01 09:24:41 +0300

    Create compare.sh

---
+++ compare.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#version 0.0.1 1.5.2017
+
+#alarm state set to 3, which is green
+ALARM=3
+#read every string from arp.list and compare, if it exist in cmdb
+while 
+if exist
+compare next
+
+if not
+write to alarm.log the new string and date and list, which is arp
+#set alarm state to 0, which is red
+ALARM=0
+compare next
+do
+
+#read every string from arp.list and compare, if it exist in cmdb
+while 
+if exist
+compare next
+
+if not
+write to alarm.log the new string and date and list, which is arp
+#set alarm state to 0, which is red
+ALARM=0
+compare next
+do
+
+./lights.sh $ALARM
