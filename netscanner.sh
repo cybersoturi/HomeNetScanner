41bff7848e07cba3e14ecf3c7d492ba0ff582a1c
cybersoturi <pasi.hakkarainen@iki.fi>
2017-05-01 08:52:53 +0300

    Update netscanner.sh

--- netscanner.sh
+++ netscanner.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-#home network scan version 1.0.4, 22.4.2017
-#identify host's LAN IPv4 address v 1.0.0
+#home network scan version 1.0.5, 1.5.2017
+#arp & nmap
 
+#defining folder V 1.0.5
+$FOLDER="/home/pi/"
+
+#arp scan for IPV4 and MAC adresses V 1.0.5
+arp |awk '/ether/{printf $1;}/ether/{print " "$3;}'|sort -V > /home/pi/arp.list
+
+#identify host's LAN IPv4 address v 1.0.0
 LAN=$(ifconfig|grep Bcast|cut -d":" -f2|cut -d" " -f1)
 
 #identify the subnet mask, can be /24 or smaller v 1.0.2
 MASK=$(ifconfig|grep Bcast|cut -d":" -f4|cut -d"." -f4)
-
 D2B=({0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1}{0..1})
 ARVO=${D2B[$MASK]}
 ARVO=$(echo $ARVO | cut -d"0" -f1 | wc -c)
 MASK=$((23+$ARVO))
-
 
 #scan all possible LAN IP address' and write a sorted list of identified IPs and MACs v 1.0.0
 nmap -sP $LAN/$MASK|awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " "$3;}'|sort -V > /home/pi/result.list
