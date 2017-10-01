7d1c2e15c8afa221ac19ff7a118e98f3a21db8ea
korpisoturi <pasi.hakkarainen@iki.fi>
2017-10-01 13:50:42 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,9 +4,11 @@
 #set up default folder
 FOLDER="/home/pi/netskanner"
 
-# starting scan, lights to yellow, scan
+# starting scan, lights to yellow
 $FOLDER/lights.sh 2
+# make nmap.list about existing IPs and MACs
 $FOLDER/netscanner.sh
+# compare to your existing cmdb.list, which you have to make by yourself
 $FOLDER/compare.sh
 # compare.sh returns value for lights.sh 3 for green and 1 for alarm
 $FOLDER/lights.sh $?
